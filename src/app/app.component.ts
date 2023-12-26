26dbe89a0a6b7625b1444b935244f02cb71490c7
Shatahir <s.taher1996@gmail.com>
2023-12-27 00:09:34 +0530

    assignment code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sketch-text';
+  values: any = [];
+  count: number = 0;
+
+  element: HTMLElement | undefined;
+
+  addvalue() {
+    this.values.push('');
+  }
+
+  changeBgColor(i: any) {
+    let _id = 'bgColor-' + i;
+    this.element = document.getElementById(_id) as HTMLElement;
+    if (this.element.classList.contains('gray')) {
+      this.element.classList.remove('gray');
+      this.count -= 1;
+    } else {
+      this.element.classList.add('gray');
+      this.count += 1;
+    }
+  }
 }
